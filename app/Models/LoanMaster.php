f2d157ce7f6011bf9c09ea6d4d9bc90315f774c7
Dhara <dharaj.gc@gmail.com>
2024-02-07 18:48:21 +0530

    changes design

--- app/Models/LoanMaster.php
+++ app/Models/LoanMaster.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\LoanEMI;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -49,5 +50,5 @@ class LoanMaster extends Model
         return $this->hasMany(LoanEMI::class, 'loan_master_id', 'id');
     }
 
-    
+
 }
